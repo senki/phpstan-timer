f9e1b584401ce96a83e41673af15220e1dee1258
Csaba Maulis <hello@senki.xyz>
2023-01-26 22:50:45 +0800

    Make code more idiomatic
    - use eyre (simple-eyre) to handle and report errors. 
    - remove premature optimisations:
        - use regex as is without "lazy_static"
        - remove variable drops 
    - add clippy config

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,99 @@
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::env;
-use std::io::{BufRead, BufReader, Lines};
-use std::{fs::File, path::Path};
+#![deny(clippy::pedantic)]
+#![deny(clippy::nursery)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::cargo)]
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+use regex::Regex;
+use simple_eyre::eyre::{bail, Result};
+use std::env::args;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
+
+fn main() -> Result<()> {
+    simple_eyre::install()?;
+
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        bail!("Missing file path argument.");
+    }
     let file_path = &args[1];
 
     let mut files: Vec<(f64, String)> = Vec::new();
     let mut filename: Option<String> = None;
-    let mut line_string = "".to_string();
+    let mut line_string: String;
 
-    for line in read_lines(file_path) {
-        line_string = line.unwrap();
-        let mut line_str = line_string.as_str();
-        line_str = trim(line_str);
+    for (index, line) in read_lines(file_path)?.enumerate() {
+        line_string = match line {
+            Ok(line) => line,
+            Err(error) => bail!("Could not read line {index} >> {error}"),
+        };
+        let line_str = trim(line_string.as_str());
 
-        if line_str == "" {
+        if line_str.is_empty() {
             continue;
         }
 
-        if filename == None {
+        if filename.is_none() {
             filename = Some(line_str.to_string());
             continue;
         }
 
-        let duration = get_duration(line_str);
-        files.push((duration, filename.unwrap()));
+        let duration = match get_duration(line_str) {
+            Ok(duration) => duration,
+            Err(error) => bail!("Could not get `duration` from line {index} >> {error}"),
+        };
+        if let Some(name) = filename {
+            files.push((duration, name));
+        }
         filename = None;
     }
 
-    drop(filename);
-    drop(line_string);
-
-    files.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+    files.sort_by(|a, b| b.0.total_cmp(&a.0));
 
     let files_length = files.len();
     let slowest_files = &files[0..if files_length < 100 { files_length } else { 100 }];
 
-    println!("Slowest files");
+    println!("Slowest files:");
     for file in slowest_files.iter() {
         println!("{:.2} seconds: {}", file.0, file.1);
     }
+
+    Ok(())
 }
 
-// SEE: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-fn read_lines<P>(filename: P) -> Lines<BufReader<File>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename).unwrap();
-    BufReader::new(file).lines()
+/// Read `file_path` one line at a time.
+///
+/// SEE: <https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html>
+fn read_lines(file_path: &String) -> Result<Lines<BufReader<File>>> {
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(error) => bail!("Could not open `{file_path}` >> {error}"),
+    };
+    Ok(BufReader::new(file).lines())
 }
 
-// Imitate PHP's trim.
-// SEE: https://www.php.net/manual/en/function.trim.php
+/// Strip whitespace from the beginning and end of a `&str`.
+/// Uses the same character list as PHP's `trim()` by default.
+///
+/// SEE: <https://www.php.net/manual/en/function.trim.php>
 fn trim(line_str: &str) -> &str {
     let chars_to_trim: &[char] = &[' ', '\t', '\n', '\r', '\0', '\u{000B}'];
     line_str.trim_matches(chars_to_trim)
 }
 
-fn get_duration(line_str: &str) -> f64 {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"took ([\d.]+) s").unwrap();
+/// Get the duration as `f64` from a `&str`.
+/// Returns `0.0` if no duration is found.
+fn get_duration(line_str: &str) -> Result<f64> {
+    let r = Regex::new(r"took ([\d.]+) s")?;
+
+    if let Some(captures) = r.captures(line_str) {
+        if let Some(matches) = captures.get(1) {
+            return match matches.as_str().parse::<f64>() {
+                Ok(duration) => Ok(duration),
+                Err(error) => bail!("`{}` not a number >> {}", matches.as_str(), error),
+            };
+        }
     }
-    let cap = RE.captures(line_str).unwrap();
-    cap.get(1).unwrap().as_str().parse::<f64>().unwrap()
+    Ok(0.0)
 }
